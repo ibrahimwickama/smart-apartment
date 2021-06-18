a6ed8be33fc9844af451a8339a5a8e0ef77c9c8d
ibrahimwickama <ibrahimwickama@gmail.com>
2021-06-18 10:57:25 +0300

    chore: add material modules

---
+++ src/app/shared/material-modules.ts
@@ -0,0 +1,33 @@
+import { MatRippleModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+export const materialModules: any[] = [
+  MatRippleModule,
+  MatFormFieldModule,
+  MatProgressBarModule,
+  MatSnackBarModule,
+  MatTooltipModule,
+  MatButtonModule,
+  MatIconModule,
+  MatRadioModule,
+  MatInputModule,
+  MatDialogModule,
+  MatMenuModule,
+  MatButtonToggleModule,
+  MatSelectModule,
+  MatStepperModule,
+  MatSidenavModule,
+];
