523b982e91bc83c3b25e5dacde5e762314a7bc9b
ibrahimwickama <ibrahimwickama@gmail.com>
2021-06-20 14:42:43 +0300

    feat: support loading of pins to map

--- src/app/shared/components/map-view/map-view.component.ts
+++ src/app/shared/components/map-view/map-view.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../../store/reducers';
@@ -21,7 +26,7 @@ export class MapViewComponent implements OnInit, AfterViewInit {
   style =
     'https://api.maptiler.com/maps/eef16200-c4cc-4285-9370-c71ca24bb42d/style.json?key=CH1cYDfxBV9ZBu1lHGqh';
   // style = `https://api.maptiler.com/maps/streets/style.json?key=${mapTilerToken}`;
-  zoom = 8;
+  zoom = 3;
 
   mapPins: any = [];
 
@@ -29,8 +34,10 @@ export class MapViewComponent implements OnInit, AfterViewInit {
     mapboxgl.accessToken = mapboxToken;
     this.store.select(fromSelectors.getMapPins).subscribe((mapPins) => {
       this.mapPins = mapPins;
-      if (mapPins.length) {
+      if (mapPins.length > 1) {
         this.buildMap();
+      } else if (mapPins.length == 1) {
+        this.zoomToMarker();
       }
     });
   }
@@ -54,19 +61,60 @@ export class MapViewComponent implements OnInit, AfterViewInit {
   // }
 
   buildMap() {
-    let map = new mapboxgl.Map({
-      container: 'map',
-      style: this.style,
-      zoom: this.zoom,
-      // center: [this.lng, this.lat],
-    });
+    try {
+      let map = new mapboxgl.Map({
+        container: 'map',
+        style: this.style,
+        zoom: 12.7,
+        // center: [this.lng, this.lat],
+      });
 
-    // add markers to map
+      // add markers to map
+      const bounds = [];
+      this.mapPins.forEach(function (markerCoordinates) {
+        // make a marker for each feature and add it to the map
+        bounds.push(
+          new mapboxgl.LngLat(markerCoordinates[0], markerCoordinates[1])
+        );
+        new mapboxgl.Marker().setLngLat(markerCoordinates).addTo(map);
+      });
 
-    this.mapPins.forEach(function (markerCoordinates) {
-      // make a marker for each feature and add it to the map
-      new mapboxgl.Marker().setLngLat(markerCoordinates).addTo(map);
-    });
-    this.map = map;
+      let llb = new mapboxgl.LngLatBounds(...bounds);
+      const centerCoordinates = llb.getCenter();
+      map.fitBounds(llb);
+      map.flyTo({
+        center: centerCoordinates,
+        essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+      });
+      this.map = map;
+    } catch (e) {}
+  }
+
+  zoomToMarker() {
+    try {
+      let map = new mapboxgl.Map({
+        container: 'map',
+        style: this.style,
+        zoom: 16,
+      });
+
+      // add markers to map
+      this.mapPins.forEach(function (markerCoordinates) {
+        // make a marker for each feature and add it to the map
+        new mapboxgl.Marker().setLngLat(markerCoordinates).addTo(map);
+      });
+
+      let sw = new mapboxgl.LngLat(this.mapPins[0][0], this.mapPins[0][1]);
+      let ne = new mapboxgl.LngLat(this.mapPins[0][0], this.mapPins[0][1]);
+      let llb = new mapboxgl.LngLatBounds(sw, ne);
+
+      map.fitBounds(llb);
+      map.flyTo({
+        center: this.mapPins[0],
+        essential: true, // this animation is considered essential with respect to prefers-reduced-motion
+      });
+
+      this.map = map;
+    } catch (e) {}
   }
 }
