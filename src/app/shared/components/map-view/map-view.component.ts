29c6229bd7eee7a4e778dbb3ec50822f2078dda3
ibrahimwickama <ibrahimwickama@gmail.com>
2021-07-04 17:28:13 +0300

    chore: handle single pin loading

--- src/app/shared/components/map-view/map-view.component.ts
+++ src/app/shared/components/map-view/map-view.component.ts
@@ -34,6 +34,7 @@ export class MapViewComponent implements OnInit, AfterViewInit {
     'https://api.maptiler.com/maps/eef16200-c4cc-4285-9370-c71ca24bb42d/style.json?key=CH1cYDfxBV9ZBu1lHGqh';
 
   mapPins: any = [];
+  markerElements: any = [];
 
   source: any;
   markers: any;
@@ -50,7 +51,7 @@ export class MapViewComponent implements OnInit, AfterViewInit {
       if (mapPins.length > 1) {
         // this.buildMap();
       } else if (mapPins.length == 1) {
-        // this.zoomToMarker();
+        this.zoomToMarker();
       }
     });
   }
@@ -205,10 +206,8 @@ export class MapViewComponent implements OnInit, AfterViewInit {
           source: 'smartApartments',
           type: 'symbol',
           layout: {
-            // 'text-field': '{message}',
             'text-size': 24,
             'text-transform': 'uppercase',
-            // 'icon-image': 'rocket-15',
             'text-offset': [0, 1.5],
           },
           paint: {
@@ -229,7 +228,7 @@ export class MapViewComponent implements OnInit, AfterViewInit {
           var el = document.createElement('div');
           el.className = 'marker';
           el.id = marker?.id;
-          el.title = JSON.stringify(marker.coordinates);
+          el['data-coordinates'] = JSON.stringify(marker.coordinates);
           el.style.backgroundImage = marker.favorite
             ? 'url(/assets/svg/pin-red-heart.svg)'
             : 'url(/assets/svg/pin-red.svg)';
@@ -248,6 +247,10 @@ export class MapViewComponent implements OnInit, AfterViewInit {
               )
             )
             .addTo(this.map);
+          this.markerElements.push({
+            ...markerElement,
+            propertyid: marker?.id,
+          });
           const markerDiv = markerElement.getElement();
           markerDiv.addEventListener('mouseenter', () =>
             markerElement.togglePopup()
@@ -257,7 +260,7 @@ export class MapViewComponent implements OnInit, AfterViewInit {
           );
           markerElement.getElement().addEventListener('click', (event) => {
             const propertyCoordinates = JSON.parse(
-              event?.srcElement['title'] || ''
+              event?.srcElement['data-coordinates'] || ''
             );
             this.map.flyTo({
               center: propertyCoordinates,
@@ -274,6 +277,48 @@ export class MapViewComponent implements OnInit, AfterViewInit {
       });
     } catch (e) {}
   }
+
+  loadMapWithPins() {
+    /// get source
+    this.source = this.map.getSource('smartApartments');
+    // let data = new FeatureCollection(markers)
+    // this.source.setData(data);
+    this.mapPins.forEach((marker) => {
+      // make a marker for each feature and add it to the map
+      // create custom marker html
+      var el = document.createElement('div');
+      el.className = 'marker';
+      el.id = marker?.id;
+      el['data-coordinates'] = JSON.stringify(marker.coordinates);
+      el.style.backgroundImage = marker.favorite
+        ? 'url(/assets/svg/pin-red-heart.svg)'
+        : 'url(/assets/svg/pin-red.svg)';
+      el.style.width = '52px';
+      el.style.height = '52px';
+      el.style.backgroundSize = '100%';
+      // create custom marker html
+
+      const markerElement = new mapboxgl.Marker(el)
+        .setLngLat(marker.coordinates)
+        .setPopup(
+          new mapboxgl.Popup().setHTML(
+            `<h4>${marker?.name}</h4>
+            <p style="color: #6c757d">
+            ${marker?.city}, ${marker?.streetAddress}
+            </p>`
+          )
+        )
+        .addTo(this.map);
+      const markerDiv = markerElement.getElement();
+      markerDiv.addEventListener('mouseenter', () =>
+        markerElement.togglePopup()
+      );
+      markerDiv.addEventListener('mouseleave', () =>
+        markerElement.togglePopup()
+      );
+    });
+  }
+
   buildMap() {
     try {
       let map = new mapboxgl.Map({
@@ -333,51 +378,16 @@ export class MapViewComponent implements OnInit, AfterViewInit {
   }
 
   zoomToMarker() {
-    const coordinates = this.mapPins[0].coordinates || [];
+    // const coordinates = this.mapPins[0].coordinates || [];
+    const focusedMarker = this.mapPins[0];
     try {
-      let map = new mapboxgl.Map({
-        container: 'map',
-        style: this.style,
-        zoom: 16,
-        center: coordinates,
+      const toRemoveMarkers = this.markerElements.filter(
+        (markerElement) => markerElement.propertyid !== focusedMarker.id
+      );
+      toRemoveMarkers.forEach((markerToRemove) => {
+        var markerElement = document.getElementById(markerToRemove.propertyid);
+        markerElement.remove();
       });
-      // let map = this.map;
-
-      // add markers to map
-      this.mapPins.forEach(function (marker) {
-        // make a marker for each feature and add it to the map
-        // create custom marker html
-        var el = document.createElement('div');
-        el.className = 'marker';
-        el.id = marker?.id;
-        el.style.backgroundImage = marker.favorite
-          ? 'url(/assets/svg/pin-red-heart.svg)'
-          : 'url(/assets/svg/pin-red.svg)';
-        el.style.width = '52px';
-        el.style.height = '52px';
-        el.style.backgroundSize = '100%';
-        // create custom marker html
-
-        const markerElement = new mapboxgl.Marker(el)
-          .setLngLat(marker.coordinates)
-          .setPopup(
-            new mapboxgl.Popup().setHTML(
-              `<h4>${marker?.name}</h4>
-              <p style="color: #6c757d">
-              ${marker?.city}, ${marker?.streetAddress}
-              </p>`
-            )
-          )
-          .addTo(map);
-        const markerDiv = markerElement.getElement();
-        markerDiv.addEventListener('mouseenter', () =>
-          markerElement.togglePopup()
-        );
-        markerDiv.addEventListener('mouseleave', () =>
-          markerElement.togglePopup()
-        );
-      });
-      this.map = map;
     } catch (e) {}
   }
 
